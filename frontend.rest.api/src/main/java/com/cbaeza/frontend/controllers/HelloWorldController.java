9657ad4577245dbbfe41b8af3c1deb1365e13dbd
cbaeza <carlos.baeza@posteo.de>
2014-01-31 22:20:15 +0100

    clean up

--- frontend.rest.api/src/main/java/com/cbaeza/frontend/controllers/HelloWorldController.java
+++ frontend.rest.api/src/main/java/com/cbaeza/frontend/controllers/HelloWorldController.java
@@ -1,8 +1,6 @@
 package com.cbaeza.frontend.controllers;
 
 import com.cbaeza.frontend.model.Hello;
-import com.cbaeza.mgmt.user.Authentication;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
